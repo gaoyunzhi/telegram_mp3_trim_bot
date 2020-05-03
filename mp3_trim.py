2041e1aed12c9679b0057e8d5b20cf0ad2f9a51c
Yunzhi Gao <gaoyunzhi@gmail.com>
2020-05-03 15:48:29 -0700

    commit

--- mp3_trim.py
+++ mp3_trim.py
@@ -2,22 +2,44 @@
 # -*- coding: utf-8 -*-
 
 import yaml
+import os
 from telegram.ext import Updater, MessageHandler, Filters
+from telegram_util import log_on_fail
+from datetime import datetime
 
 with open('credential') as f:
 	credential = yaml.load(f, Loader=yaml.FullLoader)
 
-tele = Updater(credential['bot_token'], use_context=True) # @contribute_bot
-debug_group = tele.bot.get_chat(-1001198682178)
+tele = Updater(credential['bot_token'], use_context=True) # @mp3_trim_bot
+debug_group = tele.bot.get_chat(420074357)
+
+def fillZero(d):
+	x = str(d)
+	return '0' * (2 - len(x)) + x
+
+def getOutputName():
+	n = datetime.now()   
+	return '%s_%s_%s.mp3' % (
+		fillZero(n.hour), fillZero(n.minute), fillZero(n.second))
 
 @log_on_fail(debug_group)
 def command(update, context):
 	msg = update.effective_message
 	file = msg.document
-	file = file.get_file().download('tmp/')
-	cuts = list(pic_cut.cut(file))
-	os.system('rm %s' % file)
+	print(1)
+	file = file.get_file().download()
+	print(2)
+	print(file)
+	print(3)
+	output_name = 'tmp/' + getOutputName()
+	command = '../sox-14.4.2/sox ' + file + ' ' + output_name + \
+		' silence 1 0.1 1% -1 0.1 1%'
+	print(command)
+	os.system(command)
+	msg.reply_document(open(output_name,'rb'))
 	
-tele.dispatcher.add_handler(MessageHandler(Filters.document, command))
-tele.start_polling()
-tele.idle()
+if __name__ == "__main__":
+	print('===start===')
+	tele.dispatcher.add_handler(MessageHandler(Filters.audio, command))
+	tele.start_polling()
+	tele.idle()
